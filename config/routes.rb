98f4581f51a45a33213eaf6fe03a45242605364d
lltheis <theis.laura.l@gmail.com>
2016-07-13 18:53:31 +0000

    adding home & about pages, formatting blog post pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/index'
+
   resources :articles
 
-  get 'articles/index'
+  get 'welcome/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'articles#index'
+  root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
